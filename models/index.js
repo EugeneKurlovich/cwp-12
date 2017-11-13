052c608fbac6c3040eba9130a73ebb9dd127d6ba
Eugene Kurlovich <zhenikpggkurlovich@gmail.com>
2017-11-13 11:18:48 +0300

    (turtle, weapon, pizza) models & create comm

--- models/index.js
+++ models/index.js
@@ -3,14 +3,25 @@ const Weapon = require('./weapon');
 const Pizza = require('./pizza');
 
 module.exports = (Sequelize, config) => {
-  // TODO: создание объекта для подключения к базе - sequelize
+ const sequelize = new Sequelize(config.db, config.login, config.password, {
+        host: config.host,
+        dialect: config.dialect,
+        logging: false
+    });
+    sequelize.authenticate().then(() => {
+        console.log('Connection to database successful');
+    }).catch((err) => {
+            console.log('Unable to connect to database', err);
+        });
 
   const turtles = Turtle(Sequelize, sequelize);
   const weapons = Weapon(Sequelize, sequelize);
   const pizzas = Pizza(Sequelize, sequelize);
 
-  // TODO: создание связей между таблицами
-
+    turtles.belongsTo(pizzas, {foreignKey: 'firstFavouritePizzaId', as: 'firstFavouritePizza'});
+    turtles.belongsTo(pizzas, {foreignKey: 'secondFavouritePizzaId', as: 'secondFavouritePizza'});
+    turtles.belongsTo(weapons, {foreignKey: 'weaponId', as: 'weapon'});
+    
   return {
     turtles,
     weapons,
--- models/turtle.js
+++ models/turtle.js
@@ -1,5 +1,14 @@
 module.exports = (Sequelize, sequelize) => {
   return sequelize.define('turtles', {
-    // TODO: описание полей
+      id: {    type: Sequelize.INTEGER,
+            autoIncrement: true,
+            primaryKey: true},
+     name: { type: Sequelize.STRING(500),
+   allowNull: false},
+    color:{ type: Sequelize.STRING(500),
+   allowNull: false},
+    weaponId:{type: Sequelize.INTEGER},
+    firstFavoritePizzaId:{type: Sequelize.INTEGER},
+    secondFavoritePizzaId:{type: Sequelize.INTEGER}
   });
 };
